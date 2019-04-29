b5c310989dbbce8fba99a20f1514f2457f7112c0
RuslanAmraev <46445130+RuslanAmraev@users.noreply.github.com>
2019-04-29 21:39:06 +0600

    First commit

---
+++ script/script.js
@@ -0,0 +1,9 @@
+new WOW().init();
+
+function hideModal(){
+	document.getElementById('modal').style.display = 'none'
+}
+
+function showModal(){
+	document.getElementById('modal').style.display = 'flex'
+}
